0a728a314db944372f05300ec888609ddc4f894f
jeanbd <jeankrlobd@gmail.com>
2024-09-28 15:22:50 -0500

    Calapsor todo + filtros de pendiente y completado

--- src/app/tasks/components/task-list/task-list.component.ts
+++ src/app/tasks/components/task-list/task-list.component.ts
@@ -1,16 +1,16 @@
 import { CommonModule } from '@angular/common';
 
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import tasks from '../../../../mocks/tareas.json';
 import { Task } from '../../interfaces/task.interface';
 
-import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
 
 
 @Component({
@@ -34,6 +34,45 @@ export class TaskListComponent implements OnInit {
   ngOnInit(): void {
     console.log('estos son los taks', tasks)
   }
-  dataTasks:Task[] = tasks
+  dataTasks: Task[] = tasks
+  isCollapsedAll: boolean = false;
+  completeActivated: boolean = false;
+  pendingActivated: boolean = false;
+
   @ViewChild(MatAccordion) accordion!: MatAccordion;
+
+  pendingFilter() {
+    this.dataTasks = tasks;
+    this.dataTasks = this.dataTasks.filter(e => e.estado == "Pendiente");
+    this.isCollapsedAll=false
+    this.accordion.closeAll()
+    this.pendingActivated = true;
+    this.completeActivated = false;
+    
+  }
+
+  completeFilter() {
+    this.dataTasks = tasks;
+    this.dataTasks = this.dataTasks.filter(e => e.estado == "Completada");
+    this.isCollapsedAll=false
+    this.accordion.closeAll()
+    this.pendingActivated = false;
+    this.completeActivated = true;
+    
+  }
+
+  cleanFilter() {
+    this.dataTasks = tasks;
+    this.isCollapsedAll=false
+    this.accordion.closeAll()
+    this.pendingActivated = false;
+    this.completeActivated = false
+    
+  }
+
+  collapseAll() {
+    this.isCollapsedAll = !this.isCollapsedAll
+    this.isCollapsedAll == true ? this.accordion.openAll() : this.accordion.closeAll()
+  }
+
 }
